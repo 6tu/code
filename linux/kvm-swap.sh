02e88668141b5b36a68ff5f7d5170a1b0a341365
yisuo <yourshell@users.noreply.github.com>
2018-05-04 10:04:13 +0800

    Update kvm-swap.sh

--- linux/kvm-swap.sh
+++ linux/kvm-swap.sh
@@ -1,4 +1,11 @@
-dd if=/dev/zero of=/home/swap bs=1024 count=512000
+swapoff /home/swap
+dd if=/dev/zero of=/home/swap bs=1024 count=256000
 /sbin/mkswap -f /home/swap
+
 /sbin/swapon -f /home/swap
 vim /etc/fstab
+/tmp/swap swap swap defaults 0 0
+
+swapon -s
+
+UUID=7799f635-1ec9-4235-929d-68fcc24e9272
