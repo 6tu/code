113723f69d7dbafdac35c2c426eabce991f9e1a8
yisuo <yourshell@users.noreply.github.com>
2018-05-05 19:40:56 +0800

    Update ubuntu-kernel.sh

--- linux/ubuntu/ubuntu-kernel.sh
+++ linux/ubuntu/ubuntu-kernel.sh
@@ -12,15 +12,14 @@ reboot
 # 用 apt-get purge 删除，不出现deinstall的情况
 # 删了旧内核后，用sudo update-grub更新一下启动引导
 
-apt-get purge -y linux-headers-4.4.*
-apt-get purge -y linux-image-4.4.*
-apt-get purge -y linux-image-extra-4.4.*
+apt-get purge -y "linux-headers-4.4.*"
+apt-get purge -y "linux-image-4.4.*"
+apt-get purge -y "linux-image-extra-4.4.*"
 
 # dpkg --get-selections| grep linux
-# apt-get autoremove -y linux-headers-4.4.*
-# apt-get autoremove -y linux-image-4.4.*
-# apt-get autoremove -y linux-image-extra-4.4.*
+# apt-get autoremove -y "linux-headers-4.4.*"
+# apt-get autoremove -y "linux-image-extra-4.4.*"
 # 
-# dpkg -P linux-headers-4.4.*
+# dpkg -P "linux-headers-4.4.*"
 # dpkg -P linux-image-4.4.0-112-generic
 
