19a7ee74a5faf1ab5ba35c2e4e0d67b3bc26de00
yisuo <yourshell@users.noreply.github.com>
2018-05-07 09:19:20 +0800

    Add files via upload

---
+++ linux/iptables/lampp-ikev2.sh
@@ -0,0 +1,9 @@
+
+cd ~
+wget https://raw.githubusercontent.com/6tu/code/master/linux/xampp/lampp.sh
+wget https://raw.githubusercontent.com/6tu/code/master/linux/quericy-one-key-ikev2.sh
+chmod +x ~/lampp.sh ~/quericy-one-key-ikev2.sh
+mv ~/quericy-one-key-ikev2.sh ~/vpn
+~/lampp.sh
+~/vpn/quericy-one-key-ikev2.sh
+
