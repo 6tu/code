4f7031b673e797f6a9cd6a143f766dc287abb8eb
liuyun <yourshell@users.noreply.github.com>
2018-05-30 18:28:46 +0800

    Update cp.sh

--- php/hosts/cp.sh
+++ php/hosts/cp.sh
@@ -1,3 +1,4 @@
-sudo /bin/cp /home/wwwroot/default/hosts.allow /etc/hosts.allow
+webroot=/var/www
+sudo /bin/cp ${webroot}/hosts/hosts.allow /etc/hosts.allow
 sudo /bin/chown root:root /etc/hosts.allow
-sudo /bin/rm -rf /home/wwwroot/default/hosts.allow
+sudo /bin/rm -rf ${webroot}/hosts/hosts.allow
