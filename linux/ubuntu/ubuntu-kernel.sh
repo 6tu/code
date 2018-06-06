d8e519c2ffa491970d0e9bea4b88f396bf97b59f
liuyun <yourshell@users.noreply.github.com>
2018-06-06 18:14:05 +0800

    Update ubuntu-kernel.sh

--- linux/ubuntu/ubuntu-kernel.sh
+++ linux/ubuntu/ubuntu-kernel.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y libelf1
-
+cd
 mkdir kernel && cd kernel
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.1/linux-headers-4.15.1-041501_4.15.1-041501.201802031831_all.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.1/linux-headers-4.15.1-041501-generic_4.15.1-041501.201802031831_amd64.deb
@@ -8,13 +8,15 @@ wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.1/linux-image-4.15.1-04
 dpkg -i linux-image-4.15.1-041501-generic_4.15.1-041501.201802031831_amd64.deb
 dpkg -i linux-headers-4.15.1-041501_4.15.1-041501.201802031831_all.deb
 dpkg -i linux-headers-4.15.1-041501-generic_4.15.1-041501.201802031831_amd64.deb
-reboot
-# 用 apt-get purge 删除，不出现deinstall的情况
-# 删了旧内核后，用sudo update-grub更新一下启动引导
 
 apt-get purge -y "linux-headers-4.4.*"
 apt-get purge -y "linux-image-4.4.*"
 apt-get purge -y "linux-image-extra-4.4.*"
+update-grub
+rm -rf ~/*.deb ~/kernel
+reboot
+# 用 apt-get purge 删除，不出现deinstall的情况
+# 删了旧内核后，用sudo update-grub更新一下启动引导
 
 # dpkg --get-selections| grep linux
 # apt-get autoremove -y "linux-headers-4.4.*"
