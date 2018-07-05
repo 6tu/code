c99fb5eb350be3cf1d23ab3e28465293c594a41b
liuyun <yourshell@users.noreply.github.com>
2018-07-05 17:39:48 +0800

    Update denyssh.sh

--- linux/centos/denyssh.sh
+++ linux/centos/denyssh.sh
@@ -1,31 +1,52 @@
 #!/bin/bash
-yum -y install  vixie-cron crontabs
-mkdir -p /usr/local/cron/
+
 cat > /usr/local/cron/sshdeny.sh << "EOF"
 #!/bin/bash
 DEFINE="3"
-cat /var/log/secure|awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2"="$1;}'>/tmp/sshDenyTemp.txt
+cat /var/log/secure|awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2"="$1;}' > /tmp/sshDenyTemp.txt
 for i in `cat /tmp/sshDenyTemp.txt`
 do
-IP=`echo $i |awk -F= '{print $1}'`
-NUM=`echo $i|awk -F= '{print $2}'`
-if [ $NUM -gt $DEFINE ];
-then
-grep $IP /etc/hosts.deny > /dev/null
-if [ $? -gt 0 ];
-then
-echo "sshd:$IP" >> /etc/hosts.deny
-fi
-fi
+    IP=`echo $i |awk -F= '{print $1}'`
+    NUM=`echo $i|awk -F= '{print $2}'`
+    if [ $NUM -gt $DEFINE ];
+    then
+        grep $IP /etc/hosts.deny > /dev/null
+        if [ $? -gt 0 ];
+        then
+            echo "sshd:$IP" >> /etc/hosts.deny
+        fi
+    fi
 done
 echo > /var/log/secure
 rm -rf /tmp/sshDenyTemp.txt
+#echo sshd>> /root/ssh.log
 EOF
+
+Install_cron()
+{
+    if [ "$PM" = "yum" ]; then
+        yum -y install  vixie-cron crontabs
+        log=/var/log/secure
+        mkdir -p /usr/local/cron
+        echo '*/10 * * * * /bin/bash /usr/local/cron/sshdeny.sh > /dev/null 2>&1' >> /var/spool/cron/root
+        crontab /var/spool/cron/root
+        chmod 600 /var/spool/cron/root
+    elif [ "$PM" = "apt" ]; then
+        apt -y update
+        apt install -y cron
+        log=/var/log/auth.log
+        sed -i 's/secure/auth.log/g' /usr/local/cron/sshdeny.sh
+        mkdir -p /usr/local/cron/crontabs
+        echo '*/10 * * * * /bin/bash /usr/local/cron/sshdeny.sh > /dev/null 2>&1' >> /var/spool/cron/crontabs/root
+        crontab /var/spool/cron/crontabs/root
+        chmod 600 /var/spool/cron/crontabs/root
+    fi
+}
+Install_cron
 chmod +x /usr/local/cron/sshdeny.sh
-echo '*/60 * * * * /usr/local/cron/sshdeny.sh > /dev/null 2>&1' >> /var/spool/cron/root
-chmod 600 /var/spool/cron/root
+
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Deny for SSH Cront have added success!"
-echo "The task work by 5/min"
+echo "The task work by 10/min"
 echo "If you want to allow one, please delete it from /etc/hosts.deny"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
