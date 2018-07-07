917db3b0a7e7b49e62149e9bd8c9e67ddfe2fa67
liuyun <yourshell@users.noreply.github.com>
2018-07-07 13:19:11 +0800

    Update centos7.sh

--- linux/centos/centos7.sh
+++ linux/centos/centos7.sh
@@ -22,7 +22,6 @@ echo "sshd:all" >> /etc/hosts.deny
 
 # 系统升级
 echo "" && echo "======== system update ========" && echo ""
-yum -y update
 # 提供额外的软件包
 yum install -y epel-release
 yum -y update
@@ -42,20 +41,20 @@ ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 # 安装编译环境和依赖库
 echo "" && echo "======== install Development Tools ========" && echo ""
-yum install -y wget curl git vim zip unzip screen nohup dos2unix
-yum install -y dmidecode deltarpm redhat-lsb ntpdate ntp vixie-cron crontabs
-yum install -y whois net-tools redhat-lsb ca-certificates
+yum install -y vim dos2unix zip unzip tree bash-completion time
+yum install -y wget curl git lrzsz nmap nc telnet ntp ntpdate net-tools iproute bind-utils whois
+yum install -y redhat-lsb dmidecode deltarpm vixie-cron crontabs lsof psmisc screen nohup
 yum install -y zlib-devel bzip2-devel xz-devel libcurl-devel
-yum install -y openssl-devel ncurses-devel libpcap-devel
-yum install -y gcc flex bison autoconf automake
-yum reinstall -y glibc-common
-yum -y groupinstall "Development libraries" "Development tools"
-yum install -y libtool libev-devel
-yum install -y libxml2 libxml2-devel libxslt libxslt-devel xmlto asciidoc
+yum install -y libxml2 libxml2-devel libxslt libxslt-devel xmlto asciidoc man
 yum install -y libjpeg-devel libpng-devel libtiff-devel freetype-devel pam-devel gettext-devel pcre-devel
-yum install -y gpgme-devel rng-tools man
-rngd -r /dev/urandom
+yum install -y openssl openssl-devel ncurses-devel libpcap-devel ca-certificates gpgme-devel rng-tools
+yum install -y libtool udns-devel libev-devel
+yum install -y gcc flex bison autoconf automake
+yum -y groupinstall "Development libraries" "Development tools"
+yum reinstall -y glibc-common
+# yum install -y httpd mod_ssl php php-devel php-gd php-pecl-memcache php-snmp php-xmlrpc php-xml php-mbstring
 
+rngd -r /dev/urandom
 
 # 安装 Python2.7
 echo "" && echo "======== install python ========" && echo ""
