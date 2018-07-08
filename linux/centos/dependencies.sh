351efbe433a40415bffa306cc1268b1335dd5d12
liuyun <yourshell@users.noreply.github.com>
2018-07-08 17:25:20 +0800

    Update dependencies.sh

--- linux/centos/dependencies.sh
+++ linux/centos/dependencies.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-yum install -y --skip-broken vim dos2unix zip unzip tree bash-completion time
-yum install -y --skip-broken wget curl git lrzsz nmap nc telnet ntp ntpdate net-tools iproute bind-utils whois
-yum install -y --skip-broken yum-utils redhat-lsb dmidecode deltarpm vixie-cron crontabs lsof psmisc screen virt-what nohup
-yum install -y --skip-broken zlib-devel* bzip2-devel xz-devel libcurl-devel
-yum install -y --skip-broken libxml2 libxml2-devel libxslt libxslt-devel xmlto asciidoc man
-yum install -y --skip-broken libjpeg-devel libpng-devel libtiff-devel freetype-devel pam-devel gettext-devel pcre-devel
-yum install -y --skip-broken openssl-devel ncurses-devel libpcap-devel ca-certificates gpgme-devel rng-tools
-yum install -y --skip-broken libtool udns-devel libev-devel
-yum install -y --skip-broken gcc flex bison autoconf automake
 yum -y groupinstall "Development libraries" "Development tools"
+echo 3 > /proc/sys/vm/drop_caches
+yum install -y --skip-broken gcc flex bison autoconf automake
+yum install -y --skip-broken libtool udns-devel libev-devel
+yum install -y --skip-broken openssl-devel ncurses-devel libpcap-devel ca-certificates gpgme-devel rng-tools
+yum install -y --skip-broken libjpeg-devel libpng-devel libtiff-devel freetype-devel pam-devel gettext-devel pcre-devel
+yum install -y --skip-broken libxml2 libxml2-devel libxslt libxslt-devel xmlto asciidoc man
+yum install -y --skip-broken zlib-devel* bzip2-devel xz-devel libcurl-devel
+yum install -y --skip-broken yum-utils dmidecode deltarpm vixie-cron crontabs lsof psmisc screen virt-what nohup
+yum install -y --skip-broken wget curl git lrzsz nmap nc telnet ntp ntpdate net-tools iproute bind-utils whois
+yum install -y --skip-broken vim dos2unix zip unzip tree bash-completion time
+# yum install -y --skip-broken redhat-lsb
 # yum reinstall -y glibc-common
 
 # yum install -y httpd mod_ssl mod_perl php php-devel php-gd php-pecl-memcache php-snmp php-xmlrpc php-xml php-mbstring
