1a1ce05f4d937e7d9443987310f4142210da8e71
liuyun <yourshell@users.noreply.github.com>
2018-07-08 16:37:09 +0800

    Update centos-kernel.sh

--- linux/centos/centos-kernel.sh
+++ linux/centos/centos-kernel.sh
@@ -3,7 +3,6 @@ yum upgrade
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-6-8.el6.elrepo.noarch.rpm
-#yum remove kernel-headers kernel-tools kernel-tools-libs
 yum --enablerepo=elrepo-kernel install -y kernel-ml kernel-ml-devel kernel-ml-headers kernel-ml-tools kernel-ml-tools-libs kernel-ml-tools-libs-devel
 
 awk -F\’ ‘$1==”menuentry ” {print $2}’ /etc/grub.conf
@@ -18,3 +17,5 @@ rpm -qa | grep kernel
 
 # 删除旧内核的RPM包
 yum remove kernel-2.6.18-194.el5
+#yum remove kernel-headers kernel-tools kernel-tools-libs
+
