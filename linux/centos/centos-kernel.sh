7c29382f9c1cae8a854b38fbcbb6a8c37d49f867
liuyun <yourshell@users.noreply.github.com>
2018-07-09 03:16:05 +0800

    Update centos-kernel.sh

--- linux/centos/centos-kernel.sh
+++ linux/centos/centos-kernel.sh
@@ -19,3 +19,23 @@ rpm -qa | grep kernel
 yum remove kernel-2.6.18-194.el5
 #yum remove kernel-headers kernel-tools kernel-tools-libs
 
+yum install -y grub grub2
+grub2-mkconfig -o /boot/grub2/grub.cfg
+相当于UBUNTU中的 update-grub2命令
+
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum --enablerepo=elrepo-kernel install -y kernel-ml kernel-ml-devel kernel-ml-headers
+
+# wget http://elrepo.reloumirrors.net/kernel/el7/x86_64/RPMS/kernel-ml-4.17.4-1.el7.elrepo.x86_64.rpm
+# wget http://elrepo.reloumirrors.net/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.17.4-1.el7.elrepo.x86_64.rpm
+# wget http://elrepo.reloumirrors.net/kernel/el7/x86_64/RPMS/kernel-ml-headers-4.17.4-1.el7.elrepo.x86_64.rpm
+# rpm -ivh kernel-ml-4.17.4-1.el7.elrepo.x86_64.rpm
+# rpm -ivh kernel-ml-devel-4.17.4-1.el7.elrepo.x86_64.rpm
+# rpm -ivh --force /mnt/cd/Packages/kernel-3.10.0-862.e17.x86_64.rpm
+
+# 查看系统可用内核
+# cat /boot/grub2/grub.cfg |grep menuentry
+#修改开机时默认使用的内核
+# grub2-set-default 'CentOS Linux (3.10.0-327.el7.x86_64) 7 (Core)'
+
