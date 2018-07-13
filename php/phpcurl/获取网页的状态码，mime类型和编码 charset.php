8207d674e8c935aba98e197281965a03547bd197
liuyun <yourshell@users.noreply.github.com>
2018-07-13 17:55:15 +0800

    Update 获取网页的状态码，mime类型和编码 charset.php

--- php/phpcurl/获取网页的状态码，mime类型和编码 charset.php
+++ php/phpcurl/获取网页的状态码，mime类型和编码 charset.php
@@ -1,50 +1,89 @@
 <?php
-# 获取网页的状态码，mime类型和编码 charset
+# 获取网页内容，报头，状态码，mime类型和编码 charset
 
 $url = 'http://ysuo.org';
 $res_array = GetPage($url);
+echo $res_array['body'];
 
-if(empty($res_array[0])) die('<br><br><center><b>连接超时</b></center>');
-$headers = explode("\r\n", $res_array[0]);
-$status = explode(' ', $headers[0]);
-$headers[0] = str_replace('HTTP/1.1', 'HTTP/1.1:', $headers[0]);
-foreach($headers as $header){
-    if(stripos(strtolower($header), 'content-type:') !== FALSE){
-        $headerParts = explode(' ', $header);
-        $mime_type = trim(strtolower($headerParts[1]));
-        if(!empty($headerParts[2])){
-            $charset_array = explode('=', $headerParts[2]);
-            $charset = trim(strtolower($charset_array[1]));
-        }else{
-            $charset = preg_match("/<meta.+?charset=[^\w]?([-\w]+)/i",$res_array[1],$temp) ? strtolower($temp[1]):"";
-        }
-    }
-}
-# 如果$headers为空，则连接时。如果$headers第一行没有200，则连接异常。
-if($status[1] !== '200') die("连接异常，状态码： $status[1]\r\n$res_array[0]\r\n\r\n");
-// echo "<pre>\r\n$res_array[0]\r\n\r\n" . "$status[1]\r\n$mime_type\r\n$charset\r\n\r\n";
-// header($res_array[0]);
-if(strstr($mime_type,'text/html')){
-    $body = mb_convert_encoding ($res_array[1],'utf-8',$charset);
-}else $body = $res_array[1];
-# $body = preg_replace('/(?s)<meta http-equiv="Expires"[^>]*>/i', '', $body);
-echo $body;
 
-# 使用CURL取得URL内容，包括头信息
-# 简单的理解为 请求连接超时CURLOPT_CONNECTTIMEOUT
-# 响应数据传输时允许时间CURLOPT_TIMEOUT
-function GetPage($url){
+# 返回值网页内容，报头，状态码，mime类型和编码 charset
+function get_contents($url){
+    
+    if(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) $lang = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    else $lang = 'zh-CN,zh;q=0.9';
+    if(!empty($_SERVER['HTTP_USER_AGENT'])) $agent = $_SERVER['HTTP_USER_AGENT'];
+    else $agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36';
+    if(!empty($_SERVER['HTTP_REFERER'])) $refer = $_SERVER['HTTP_REFERER'];
+    else $refer = 'https://www.google.com/?gws_rd=ssl';
+    # echo "<pre>\r\n" . $agent . "\r\n" . $refer . "\r\n" . $lang . "\r\n\r\n";
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_REFERER, $refer);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept-Language: " . $lang));
+    
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);   // follow redirects
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);      // set referer on redirect
+
     curl_setopt($ch, CURLOPT_HEADER, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_TIMEOUT, 8);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,4);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($ch);
     curl_close($ch);
+    
     $res_array = explode("\r\n\r\n", $result, 2);
+    $headers = explode("\r\n", $res_array[0]);
+    $status = explode(' ', $headers[0]);
+    # 如果$headers为空，则连接超时
+    if(empty($res_array[0])) die('<br><br><center><b>连接超时</b></center>');
+    # 如果$headers状态码为404，则自定义输出页面。
+    if($status[1] == '404') die("<pre><b>找不到，The requested URL was not found on this server.</b>\r\n\r\n$res_array[0]</pre>\r\n\r\n");
+    # 如果$headers第一行没有200，则连接异常。
+    # if($status[1] !== '200') die("<pre><b>连接异常，状态码： $status[1]</b>\r\n\r\n$res_array[0]</pre>\r\n\r\n");\
+
+    if($status[1] !== '200'){
+        $body_array = explode("\r\n\r\n", $res_array[1], 2);
+        $header_all = $res_array[0] . "\r\n\r\n" . $body_array[0];
+        $res_array[0] = $body_array[0];
+        $body = $body_array[1];
+    }else{
+        $header_all = $res_array[0];
+        $body = $res_array[1];
+    }
+
+    $headers = explode("\r\n", $res_array[0]);
+    $status = explode(' ', $headers[0]);
+    
+    $headers[0] = str_replace('HTTP/1.1', 'HTTP/1.1:', $headers[0]);
+    foreach($headers as $header){
+        if(stripos(strtolower($header), 'content-type:') !== FALSE){
+            $headerParts = explode(' ', $header);
+            $mime_type = trim(strtolower($headerParts[1]));
+            if(!empty($headerParts[2])){
+                $charset_array = explode('=', $headerParts[2]);
+                $charset = trim(strtolower($charset_array[1]));
+            }else{
+                $charset = preg_match("/<meta.+?charset=[^\w]?([-\w]+)/i", $res_array[1], $temp) ? strtolower($temp[1]):"";
+            }
+        }
+    }
+
+    if(strstr($mime_type, 'text/html')){
+        $body = mb_convert_encoding ($body, 'utf-8', $charset);
+    }
+    # $body = preg_replace('/(?s)<meta http-equiv="Expires"[^>]*>/i', '', $body);    
+    
+    # echo "<pre>\r\n$header_all\r\n\r\n" . "$status[1]\r\n$mime_type\r\n$charset\r\n\r\n";
+    # header($res_array[0]);
+
+    $res_array = array();
+    $res_array['header']    = $header_all;
+    $res_array['status']    = $status[1];
+    $res_array['mime_type'] = $mime_type;
+    $res_array['charset']   = $charset;
+    $res_array['body']      = $body;
     return $res_array;
 }
-
