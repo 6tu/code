5f52a43e2bc4ffbad6050537993cacf30177a9b6
liuyun <yourshell@users.noreply.github.com>
2018-08-29 16:06:03 +0800

    定时执行器

--- php/mmh/mmh-cron.sh
+++ php/mmh/mmh-cron.sh
@@ -5,11 +5,11 @@ export PATH
 # crontab -e
 #分 时 日 月 周
 # centos
-#echo '* */1 * * * /var/www/mmh/mmh.sh > /dev/null 2>&1' >> /var/spool/cron/root
+#echo '*/10 * * * * /var/www/mmh/mmh-cron.sh > /dev/null 2>&1' >> /var/spool/cron/root
 #crontab /var/spool/cron/root
 
 # ubuntu
-#echo '* */1 * * * /var/www/mmh/mmh.sh > /dev/null 2>&1' >> /var/spool/cron/crontabs/root
+#echo '*/10 * * * * /var/www/mmh/mmh-cron.sh > /dev/null 2>&1' >> /var/spool/cron/crontabs/root
 #crontab /var/spool/cron/crontabs/root
 
 mmhpath=/var/www/mmh
@@ -18,3 +18,4 @@ test -d $mmhpath || mkdir -p $mmhpath
 # --spider 不下载任何文件。
 /usr/bin/wget -O $mmhpath/getmh.log http://127.0.0.1/mmh/getmh.php
 rm -rf $mmhpath/getmh.log
+
