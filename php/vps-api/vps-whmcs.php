734690dd2f362f96045f969a14a737b7595e6211
liuyun <yourshell@users.noreply.github.com>
2018-10-25 17:18:24 +0800

    Update vps-whmcs.php

--- php/vps-api/vps-whmcs.php
+++ php/vps-api/vps-whmcs.php
@@ -1,27 +1,32 @@
 <?php
 # 母鸡数据更新在罗马尼亚时间上午9点，把时间向前推9+个小时
 # 这样方便采集数据
-
 date_default_timezone_set('Europe/Bucharest');   # 罗马尼亚时间
+$t0 = date('ymdHis',time());
 date_default_timezone_set("America/Los_Angeles");# 洛杉矶时间
-$t = date('ymdH',time());
-$log = 'vps-' . date('ymd',time()) . '.log';
+$t1 = date('ymdHis',time());
+ echo "Bucharest: $t0 - Los_Angeles: $t1";
+
+$log_dir = './log/';
+if(!is_dir($log_dir)) mkdir($log_dir,0777);
+$log = $log_dir . 'vps-' . date('ymd',time()) . '.log';
+
+$cookie_dir = './cookie/';
+if(!is_dir($cookie_dir)) mkdir($cookie_dir,0777);
+$cookie_file = $cookie_dir . time() . '.cookie';
+setcookie("PHPSESSID", "vc0heoa6lfsi3gger54pkns152");
 
 if (file_exists($log)) {
     echo file_get_contents($log);
     exit;
 }
 
-
 $url = 'https://secure.hostsolutions.ro';
 $dologin = $url . '/dologin.php';
 $clientarea = $url . '/clientarea.php';
 $details = $clientarea . '?action=productdetails&id=5246&rrd=0&timeframe=hour&language=chinese';
+$details_json = $clientarea . '?action=productdetails&id=5246&api=json&act=vpsmanage';
 
-$cookie_dir = './cookie/';
-if(!is_dir($cookie_dir)) mkdir($cookie_dir,0777);
-$cookie_file = $cookie_dir . time() . '.cookie';
-setcookie("PHPSESSID", "vc0heoa6lfsi3gger54pkns152");
 $token = getResponse($clientarea, [], $cookie_file);
 preg_match('/<input type="hidden" name="token" value="(.*)"/U', $token, $match);
 // print_r($match);
@@ -31,11 +36,12 @@ $post['password'] = 'password';
 
 $login = getResponse($dologin, $post, $cookie_file);
 $pd = getResponse($details, $data=[], $cookie_file);
+$pd_json = getResponse($details, $data=[], $cookie_file);
 
 unlink($cookie_file);
 
-# 数据索取完毕
-
+# 数据索取完毕，先精简 $pd
+# 
 # 删除js
 $preg = "/<script[\s\S]*?<\/script>/i";
 $html = preg_replace($preg, "", $pd, -1);
@@ -74,7 +80,60 @@ $html = str_replace(" \r\n", "\r\n", $html);
 $html = trim($html);
 $html = "<pre><br>\r\n<b>產品詳情</b>\r\n" . $html;
 echo $html;
-file_put_contents($log, $html);
+echo "</pre><br>\r\n";
+file_put_contents($log, $html . "\r\n\r\n$t0" . '[Bucharest]');
+
+
+$arr = json_decode($pd_json, true);
+//print_r($arr);
+$info = array(  vps => array(
+                    title => $arr['title'],
+                    vpsid => $arr['vpsid'],
+                    vps_name => $arr['info']['vps']['vps_name'],
+                    act => $arr['act'],
+                    timenow => $arr['timenow'],
+                    disable_apicredential => $arr['disable_apicredential'],
+                    disable_backup_cp => $arr['disable_backup_cp'],
+                    enable_eu_iso => $arr['enable_eu_iso'],
+                    disable_webuzo => $arr['disable_webuzo'],
+                    osreinstall_limit => $arr['info']['vps']['osreinstall_limit'],
+                    io => $arr['info']['vps']['io'],
+                    vnc => $arr['info']['vps']['vnc'],
+                    band_suspend => $arr['info']['vps']['band_suspend'],
+                    tuntap => $arr['info']['vps']['tuntap'],
+                    ppp => $arr['info']['vps']['ppp'],
+                    network_speed => $arr['info']['vps']['network_speed'],
+                    upload_speed => $arr['info']['vps']['upload_speed'],
+                    dns_nameserver => $arr['info']['vps']['dns_nameserver'],
+                ),
+                system => array(
+                    status => $arr['info']['status'],
+                    uptime => $arr['info']['uptime'],
+                    virt => $arr['info']['vps']['virt'],
+                    hostsname => $arr['info']['vps']['hostname'],
+                    os_name => $arr['info']['vps']['os_name'],
+                    fstype => $arr['info']['os']['fstype'],
+                    filename => $arr['info']['os']['filename'],
+                    size => $arr['info']['os']['size'],
+                    ip => $arr['info']['ip']['0'],
+                    ip_count => $arr['info']['ip_count'],
+                    hostname => $arr['info']['hostname'],
+                ),
+                bandwidth => array(
+                    limit_gb => $arr['info']['bandwidth']['limit_gb'],
+                    used_gb => $arr['info']['bandwidth']['used_gb'],
+                    free_gb => $arr['info']['bandwidth']['free_gb'],
+                    percent => $arr['info']['bandwidth']['percent'],
+                    percent_free => $arr['info']['bandwidth']['percent_free'],
+                ),
+                cpu => $arr['info']['cpu'],
+                ram => $arr['info']['ram'],
+                disk => $arr['info']['disk'],
+                inodes => $arr['info']['inodes'],
+            );
+echo "<br><pre>\r\n";
+print_r($info);
+echo "</pre><br>\r\n";
 
 
 function getResponse($url, $data = [], $cookie_file = '', $timeout = 10){
@@ -113,5 +172,5 @@ function getResponse($url, $data = [], $cookie_file = '', $timeout = 10){
     catch(Exception $e){
         echo 'Caught exception:', $e -> getMessage(), "\n";
     }
-    unlink($cookie_file);
+    // unlink($cookie_file);
 }
