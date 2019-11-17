ac56fed77c1da7419171c8b857c8223008760265
liuyun <15335745+6tu-git@users.noreply.github.com>
2019-11-17 23:15:35 +0800

    Update mmh-cron.sh

--- php/mmh/mmh-cron.sh
+++ php/mmh/mmh-cron.sh
@@ -2,20 +2,48 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# crontab -e
-#分 时 日 月 周
-# centos
-#echo '*/10 * * * * /var/www/mmh/mmh-cron.sh > /dev/null 2>&1' >> /var/spool/cron/root
-#crontab /var/spool/cron/root
 
-# ubuntu
-#echo '*/10 * * * * /var/www/mmh/mmh-cron.sh > /dev/null 2>&1' >> /var/spool/cron/crontabs/root
-#crontab /var/spool/cron/crontabs/root
+/usr/bin/wget -q --spider --no-check-certificate https://www.liuyun.org/mmh/getmh.php
 
-mmhpath=/var/www/mmh
-test -d $mmhpath || mkdir -p $mmhpath
+# --spider 不下载任何文件
+# mmhpath=/home/www/mmh
+# mmhurl=https://www.liuyun.org/mmh/getmh.php
+# 
+# /usr/bin/wget --no-check-certificate -O $mmhpath/getmh.log $mmhurl
+# rm -rf $mmhpath/getmh.log
 
-# --spider 不下载任何文件。
-/usr/bin/wget --no-check-certificate -O $mmhpath/getmh.log https://ysuo.org/mmh/getmh.php
-rm -rf $mmhpath/getmh.log
+# 编辑定时器 crontab -e
+# 移除Cron作业 crontab -r
+# cat /etc/crontab
+# 分 时 日 月 周
+
+# /opt/lampp/bin/php -r 'echo file_get_contents(http://domain/cronit.php);'
+# /opt/lampp/bin/php /home/www/cronit.php
+# 
+# wget -q --spider http://domain/cronit.php
+# wget -O /dev/null http://domain/cronit.php
+# wget -O- http://domain/cronit.php >> /dev/null
+
+
+# 以下命令用于添加定时器，添加后注释掉
+# 
+# shellpath=/home/www/mmh
+#
+# test -d $shellpath || mkdir -p $shellpath
+# if [ ! -f "/usr/bin/yum" ]; then
+#   echo Ubuntu
+#   aptyum=apt-get
+#   apt -y install cron
+#   service cron start
+#   echo "* */1 * * * $shellpath/mmh-cron.sh > /dev/null 2>&1" >> /var/spool/cron/crontabs/root
+#   crontab /var/spool/cron/crontabs/root
+# else
+#   echo CentOS
+#   aptyum=yum
+#   yum -y install crontabs vixie-cron
+#   /bin/systemctl start crond.service
+#   /sbin/service crond start
+#   echo "* */1 * * * $shellpath/mmh-cron.sh > /dev/null 2>&1" >> /var/spool/cron/root
+#   crontab /var/spool/cron/root
+# fi
 
